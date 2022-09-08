1e13587ccbc4ff9f4894b1b68d66a3ec0509faae
Brian Nzangi <musyokiphoesolse@gmail.com>
2022-09-08 15:11:18 -0700

    Size not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - print the string in 
+ * main - entry point
  *
- * Return:  Always 0
+ * Return: Always 0
  */
 int main(void)
 {
@@ -11,10 +11,11 @@ int i;
 long li;
 long long lli;
 float f;
+
 printf("Size of a char: %ld byte(s)\n", sizeof(c));
 printf("Size of an int: %ld byte(s)\n", sizeof(i));
 printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-printf("Size of a float: %ld byte(s)\n", sizeof(f));
+printf("Size of a long long int : %ldbyte(s)\n", sizeof(lli));
+printf("Size of a float: %ld byte(s)\n", sizeof (f));
 return (0);
 }
