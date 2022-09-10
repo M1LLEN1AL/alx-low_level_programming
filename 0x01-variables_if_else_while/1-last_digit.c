5447283c6481be2216fe438bab4c2f44429146bb
Brian Nzangi <musyokiphoesolse@gmail.com>
2022-09-09 19:38:13 -0700

    Positive and negcheck

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,12 +18,12 @@ int main(void)
 
 	if (L > 5)
 		printf('%s %d is %d
-and is greater than 5\n', str, n, L);
+ and is greater than 5\n', str, n, L);
 	else if (L == 0)
-		printf('%s %d is %d
-and is 0\n', str, n. L);
+		printf('%s %d id %d
+ and is 0\n', str, n, L);
 	else if (L < 6)
-		printf('%s %d is
+	printf('%s %d is
 %d and is less than 6 and not 0\n'
 , str, n, L);
 
