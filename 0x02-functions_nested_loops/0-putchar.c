70caaf64623f55c7ba9083b8192b796754f96fd3
Brian Nzangi <musyokiphoesolse@gmail.com>
2022-09-14 02:23:44 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main -entry
+ *
+ * Return: int
+ */
+int main(void)
+{
+int n = 0;
+char str_ptc[8] = "Putchar"
+
+while (n < 8)
+{
+	_putchar(str_ptc[n]);
+	n += 1;
+}
+	_putchar('\n');
+
+	return (0);
+}
