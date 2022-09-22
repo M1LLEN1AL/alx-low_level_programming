e65c3a3ae75dfbd796a3105aab7642b71ec6d081
Brian Nzangi <musyokiphoesolse@gmail.com>
2022-09-22 13:10:53 -0700

    Task 101 commit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,19 +3,21 @@
 /**
  * print_number - Prints an integer.
  * @n: The integer to be printed
+ *
+ * Return: 0
  */
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int x;
 
 	if (n < 0)
 	{
 		_putchar('-')
-		num = -num;
+		n  *= -1;
 	}
+	x = n;
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
+	if (x / 10)
+		print_number(x / 10);
 
-	_putchar((num % 10) + '0');
 }
